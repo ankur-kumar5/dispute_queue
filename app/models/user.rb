0b44210511957b6d214f722c1301589e31b76c09
ankur-kumar5 <134390248+ankur-kumar5@users.noreply.github.com>
2026-02-16 18:34:08 +0530

    Setup User RBAC

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :sessions, dependent: :destroy
+  # EUMS
+  enum :role, {
+    admin: "admin",
+    reviewer: "reviewer",
+    read_only: "read_only"
+  }, validate: true
 
+  # VALIDATIONS
+  validates :role, presence: true
+  # ASSOCIATIONS
+  has_many :sessions, dependent: :destroy
+  # Email address normalization
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
